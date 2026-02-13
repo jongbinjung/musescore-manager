09924d9504af7be555bc761957e82d95d35ea536
Jongbin Jung <jungjongbin86@gmail.com>
2026-02-12 20:40:53 -0800

    Create global CLI options

--- src/msm/main.py
+++ src/msm/main.py
@@ -21,15 +21,31 @@ def _validate_mscz_directory(path: Path | None):
         raise typer.Exit(code=1)
 
 
-@app.command()
-def normalize(
-    path: Annotated[Path | None, typer.Argument(help="Path to a directory or a single .mscz file to normalize")] = None,
-    profile: Annotated[str, typer.Argument(help="Configuration profile to use")] = "default",
+@app.callback()
+def global_args(
+    ctx: typer.Context,
+    path: Annotated[Path | None, typer.Option(help="Path to a directory or a single .mscz file to run on")] = None,
+    profile: Annotated[str, typer.Option(help="Configuration profile to use")] = "default",
     verbose: Annotated[bool, typer.Option("--verbose", "-v")] = False,
-    dryrun: Annotated[bool, typer.Option(help="Don't actually change filenames")] = False,
+    dryrun: Annotated[bool, typer.Option(help="Don't actually execute changes")] = False,
 ):
-    configs = Configs()
-    mscz_dir = path or Configs().local_mscz_directory()
+    """Global options and arguments"""
+    configs = Configs(profile_name=profile)
+    musescore = Musescore(configs=configs)
+
+    ctx.ensure_object(dict)
+    ctx.obj["dryrun"] = dryrun
+    ctx.obj["musescore"] = musescore
+    ctx.obj["path"] = path or configs.local_mscz_directory()
+    ctx.obj["verbose"] = verbose
+
+
+@app.command()
+def normalize(ctx: typer.Context):
+    dryrun = ctx.obj.get("dryrun", True)
+    mscz_dir = ctx.obj.get("path", Configs().local_mscz_directory())
+    musescore = ctx.obj.get("musescore", Musescore())
+    verbose = ctx.obj.get("verbose", False)
 
     _validate_mscz_directory(mscz_dir)
 
@@ -41,13 +57,11 @@ def normalize(
         typer.echo(f"{mscz_dir} is not a valid mscz file or directory")
         raise typer.Exit(code=1)
 
-    musescore = Musescore(configs=configs)
-
     with Progress() as progress:
         task = progress.add_task("Normalizing scores...", total=len(mscz_paths))
 
         for _path in mscz_paths:
-            if verbose:
+            if verbose or dryrun:
                 progress.console.print(f"Normalizing {_path}")
 
             if not dryrun:
